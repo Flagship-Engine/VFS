2e610f1257817f4a290e4507ed25ec8df0a949d5
Zachary Wells <8bitslime@gmail.com>
2020-08-30 23:13:55 -0700

    Added owned version of VfsPath
    Began work on VfsPathBuf, an owned version of VfsPath.

--- src/path.rs
+++ src/path.rs
@@ -1,14 +1,20 @@
+use std::borrow::Borrow;
+use std::iter::FromIterator;
+use std::ops::Deref;
 
-pub struct VfsPath<'a>(&'a str);
+#[derive(Debug, Eq, PartialEq)]
+pub struct VfsPath(str);
 
-impl<'a> VfsPath<'a> {
-    pub fn new(string: &'a str) -> Self {
-        Self(string)
+impl VfsPath {
+    pub fn new<S: AsRef<str> + ?Sized>(s: &S) -> &Self {
+        // Copy pasta from std::path::Path
+        // Converts `&str` to `&VfsPath`, a very thin wrapper.
+        unsafe { &*(s.as_ref() as *const str as *const VfsPath) }
     }
     
-    // TODO: do dot-files have extensions?
+    // TODO: do dotfiles have extensions?
     // Windows says file: ".txt" is a text file
-    pub fn extension(&self) -> Option<&'a str> {
+    pub fn extension(&self) -> Option<&str> {
         self.iter()
             .last()
             .and_then(|last| {
@@ -18,12 +24,63 @@ impl<'a> VfsPath<'a> {
             })
     }
     
-    pub fn iter(&self) -> impl Iterator<Item = &'a str> {
+    pub fn iter(&self) -> impl Iterator<Item = &str> {
         self.0
             .split('/')
             .filter(|s| !s.is_empty())
     }
 }
+
+impl ToOwned for VfsPath {
+    type Owned = VfsPathBuf;
+    fn to_owned(&self) -> Self::Owned {
+        VfsPathBuf(self.0.to_owned())
+    }
+}
+
+#[derive(Debug, Default, Eq, PartialEq)]
+pub struct VfsPathBuf(String);
+
+impl VfsPathBuf {
+    pub fn new() -> Self { Default::default() }
+}
+
+impl Deref for VfsPathBuf {
+    type Target = VfsPath;
+    fn deref(&self) -> &Self::Target {
+        VfsPath::new(&self.0)
+    }
+}
+impl Borrow<VfsPath> for VfsPathBuf {
+    fn borrow(&self) -> &VfsPath {
+        self.deref()
+    }
+}
+
+impl From<&str> for VfsPathBuf {
+    fn from(string: &str) -> Self {
+        Self(string.to_owned())
+    }
+}
+impl From<String> for VfsPathBuf {
+    fn from(string: String) -> Self {
+        Self(string)
+    }
+}
+impl<'a, S: Deref<Target = &'a str>> FromIterator<S> for VfsPathBuf {
+    fn from_iter<T> (iter: T) -> Self
+        where T: IntoIterator<Item = S>
+    {
+        let mut buf = Self::new();
+        for s in iter {
+            buf.0.push('/');
+            buf.0.push_str(&s);
+        }
+        buf
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
@@ -65,5 +122,12 @@ mod tests {
         let path = VfsPath::new("//really/long///path.rs");
         let collect = path.iter().collect::<Vec<&str>>();
         assert_eq!(collect, vec!["really", "long", "path.rs"])
+    }
+    
+    #[test]
+    fn collect_into_vfs_path_buf() {
+        let vec = vec!["hello", "world", "file.txt"];
+        let path: VfsPathBuf = vec.iter().collect();
+        assert_eq!(path, VfsPathBuf::from("/hello/world/file.txt"))
     }
 }
